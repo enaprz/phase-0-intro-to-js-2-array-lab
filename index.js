1d5962084a917bbeb7af421a849511095847ebcf
enaprz <enasheccid7@gmail.com>
2023-10-18 18:23:30 -0700

    finished lab

--- index.js
+++ index.js
@@ -1,1 +1,37 @@
 // Write your solution here!
+const cats = ["Milo", "Otis", "Garfield"];
+function destructivelyAppendCat(){
+    cats.push("Ralph");
+    return cats;
+}
+function destructivelyPrependCat(){
+    cats.unshift("Bob");
+    return cats;
+}
+function destructivelyRemoveLastCat(){
+    cats.pop();
+    return cats;
+}
+function destructivelyRemoveFirstCat(){
+    cats.shift();
+    return cats;
+}
+function appendCat(){
+    const appendCat = cats.slice();
+    appendCat.push("Broom")
+    return appendCat;
+}
+function prependCat(){
+    const prependCat = ["Arnold",...cats]
+    return prependCat;
+}
+function removeLastCat(){
+   const removeLastCat =  cats.slice(0,-1)
+    return removeLastCat;
+}
+function removeFirstCat(){
+    const removeFirstCat = cats.slice(1)
+    return removeFirstCat
+}
+
+
